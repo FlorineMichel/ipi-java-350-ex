55ea3c1fd9f1cbe7430bc933d567c362644019f9
Florine Michel <florinemichel@orange.fr>
2021-02-22 20:35:47 +0100

    fix avgPerformanceWhereMatriculeStartsWith

--- src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryIntegrationTest.java
+++ src/test/java/com/ipiecoles/java/java350/repository/EmployeRepositoryIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.ipiecoles.java.java350.repository;
 
 import com.ipiecoles.java.java350.model.Employe;
-import com.ipiecoles.java.java350.service.EmployeService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,13 +19,14 @@ class EmployeRepositoryIntegrationTest {
     @Test
     void testAvgPerformanceWhereMatriculeStartsWith(){
 
-        employeRepository.save(new Employe("Dean", "John", "T12345", LocalDate.now(), 1500d, 1, 1.0));
-        employeRepository.save(new Employe("Joe", "Dohn", "M55555", LocalDate.now(), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Dean", "John", "C12345", LocalDate.now(), 1500d, 3, 1.0));
+        employeRepository.save(new Employe("Joe", "Dohn", "C55555", LocalDate.now(), 1500d, 5, 1.0));
         employeRepository.save(new Employe("Doe", "John", "C77777", LocalDate.now(), 1500d, 1, 1.0));
+        employeRepository.save(new Employe("Doe", "John", "C77777", LocalDate.now(), 1500d, 3, 1.0));
 
         Double avgPerf = employeRepository.avgPerformanceWhereMatriculeStartsWith("C");
 
-        Assertions.assertThat(avgPerf).isEqualTo(1);
+        Assertions.assertThat(avgPerf).isEqualTo(3);
     }
 
     @BeforeEach
